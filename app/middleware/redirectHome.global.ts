1b4b639360e85cb70dc6aa2b67f7bf850b3c77ec
Nico Wu <>
2025-11-04 17:57:31 +0800

    fix:debug route error

--- app/middleware/redirectHome.global.ts
+++ app/middleware/redirectHome.global.ts
@@ -1,10 +1,19 @@
 export default defineNuxtRouteMiddleware((to, from) => {
+  
+  const { logout } = useAuth();
   const mainStore = useMainStore();
+
+  if (mainStore.isAuthenticated && mainStore.user === null) {
+    logout();
+  }
+
   if (to.fullPath === '/') {
     return navigateTo('/home')
   };
+
   const isTabBarCorrect = to.path.includes(mainStore.tabBarActive);
   if (!isTabBarCorrect) {
     mainStore.setTabBarActive(to.href.split('/').join(''));
   };
+  
 })
