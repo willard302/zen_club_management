30f7e363a64164c77a86c596089b613853b7b832
Nico Wu <>
2025-11-08 17:58:39 +0800

    add:新增共用套件UserForm

---
+++ app/components/UserForm.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps<{
+  userData: Record<string, any>,
+  editable?: boolean
+}>();
+
+defineEmits(['update', 'submit'])
+</script>
+
+<template>
+  <div>
+    Component: UserForm
+  </div>
+</template>
+
+<style scoped></style>
